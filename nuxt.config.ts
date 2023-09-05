dad2b255b79e293c09205f763841b1e3b4d5c0c1
unknown <21bogdan09@gmail.com>
2023-09-05 14:04:41 +0300

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/tailwindcss'
-    ],
-    build: {
-        transpile: [
-            "vee-validate/dist/rules"
-        ],
+  app: {
+    baseURL: "/add-user/",
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+  build: {
+    transpile: ["vee-validate/dist/rules"],
+  },
+  // postcss: {
+  //     plugins: {
+  //         tailwindcss: {},
+  //         autoprefixer: {},
+  //     },
+  // },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_URL,
     },
-    // postcss: {
-    //     plugins: {
-    //         tailwindcss: {},
-    //         autoprefixer: {},
-    //     },
-    // },
-    runtimeConfig: {
-        public: {
-            apiBase: process.env.API_URL
-        }
-    },
-    components: [
-        '~/components',
-        { path: '~/components/layouts' },
-        { path: '~/components/domains' },
-        { path: '~/components/ui' },
-    ],
-})
+  },
+  components: [
+    "~/components",
+    { path: "~/components/layouts" },
+    { path: "~/components/domains" },
+    { path: "~/components/ui" },
+  ],
+});
